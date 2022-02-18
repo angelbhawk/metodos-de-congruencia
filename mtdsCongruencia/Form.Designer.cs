36830ba812f7f507aae4d32e2a9b18fb832e5f41
Miguel Angel Murillo Jaimes <L19310937@lapaz.tecnm.mx>
2022-02-18 14:52:00 -0700

    Agregar archivos de proyecto.

---
+++ mtdsCongruencia/Form.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace mtdsCongruencia
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbxMetodos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudA = new System.Windows.Forms.NumericUpDown();
+            this.nudB = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudM = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nudX = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.nudN = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(211, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(285, 264);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cbxMetodos
+            // 
+            this.cbxMetodos.FormattingEnabled = true;
+            this.cbxMetodos.Items.AddRange(new object[] {
+            "Método Congruencial Mixto",
+            "Método Congruencial Multiplicativo"});
+            this.cbxMetodos.Location = new System.Drawing.Point(12, 31);
+            this.cbxMetodos.Name = "cbxMetodos";
+            this.cbxMetodos.Size = new System.Drawing.Size(166, 21);
+            this.cbxMetodos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecionar método de generación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tabla de números";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese valor en A";
+            // 
+            // nudA
+            // 
+            this.nudA.Location = new System.Drawing.Point(12, 81);
+            this.nudA.Name = "nudA";
+            this.nudA.Size = new System.Drawing.Size(166, 20);
+            this.nudA.TabIndex = 5;
+            // 
+            // nudB
+            // 
+            this.nudB.Location = new System.Drawing.Point(12, 120);
+            this.nudB.Name = "nudB";
+            this.nudB.Size = new System.Drawing.Size(166, 20);
+            this.nudB.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ingrese valor en B";
+            // 
+            // nudM
+            // 
+            this.nudM.Location = new System.Drawing.Point(12, 159);
+            this.nudM.Name = "nudM";
+            this.nudM.Size = new System.Drawing.Size(166, 20);
+            this.nudM.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ingrese valor en M";
+            // 
+            // nudX
+            // 
+            this.nudX.Location = new System.Drawing.Point(12, 198);
+            this.nudX.Name = "nudX";
+            this.nudX.Size = new System.Drawing.Size(166, 20);
+            this.nudX.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Ingrese valor en X";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(12, 272);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(166, 23);
+            this.btnGenerar.TabIndex = 12;
+            this.btnGenerar.Text = "Generar números";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // nudN
+            // 
+            this.nudN.Location = new System.Drawing.Point(12, 237);
+            this.nudN.Name = "nudN";
+            this.nudN.Size = new System.Drawing.Size(166, 20);
+            this.nudN.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Cantidad a generar";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "i";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Xi";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.HeaderText = "Número generado";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 310);
+            this.Controls.Add(this.nudN);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.nudX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nudM);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nudB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxMetodos);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form";
+            this.Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbxMetodos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudM;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nudX;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.NumericUpDown nudN;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ mtdsCongruencia/Form.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mtdsCongruencia
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        double[] resultados;
+
+        public Form()
+        {
+            InitializeComponent();
+            nudX.Value = 1;
+            nudA.Value = 6;
+            nudB.Value = 9;
+            nudM.Value = 13;
+            nudN.Value = 12;
+            cbxMetodos.SelectedIndex = 0;
+            //MessageBox.Show("resultado = "+getXi(1,6,9,13));
+        }
+
+        private void metodoMixto(int numero)
+        {
+            int x = Convert.ToInt32(nudX.Value);
+            int a = Convert.ToInt32(nudA.Value);
+            int b = Convert.ToInt32(nudB.Value);
+            int m = Convert.ToInt32(nudM.Value);
+            int xi = getXiMixto(x, a, b, m);
+
+            for (int i = 0; i < numero; i++) 
+            {
+                resultados[i] = getUiMixto(xi, m);
+                //MessageBox.Show("Ui = " + resultados[i] + " con Xi = "+xi);
+                dataGridView1.Rows.Add(i+1, xi, resultados[i]);
+                xi = getXiMixto(xi, a, b, m);
+            }
+        }
+
+        private void metodoMultiplicativo(int numero)
+        {
+            int x = Convert.ToInt32(nudX.Value);
+            int a = Convert.ToInt32(nudA.Value);
+            int b = Convert.ToInt32(nudB.Value);
+            int m = Convert.ToInt32(nudM.Value);
+            int xi = getXiMultiplicativo(x, a, b, m);
+
+            for (int i = 0; i < numero; i++)
+            {
+                resultados[i] = getUiMultiplicativo(xi, m);
+                //MessageBox.Show("Ui = " + resultados[i] + " con Xi = "+xi);
+                dataGridView1.Rows.Add(i + 1, xi, resultados[i]);
+                xi = getXiMultiplicativo(xi, a, b, m);
+            }
+        }
+
+        private int getXiMixto(int x, int a, int b, int m) 
+        {
+            return (x*a+b)%m;
+        }
+
+        private double getUiMixto(int xi, int m)
+        {
+            return (double)xi /m;
+        }
+
+        private int getXiMultiplicativo(int x, int a, int b, int m)
+        {
+            return (x * a) % m;
+        }
+
+        private double getUiMultiplicativo(int xi, int m)
+        {
+            return (double)xi / (m-1);
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            int numero = Convert.ToInt32(nudN.Value);
+            resultados = new double[numero];
+
+            if (cbxMetodos.SelectedIndex == 0)
+            {
+                metodoMixto(numero);
+            }
+            else if(cbxMetodos.SelectedIndex == 1)
+            {
+                metodoMultiplicativo(numero);
+            }
+        }
+    }
+}
